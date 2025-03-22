e71d1824153a4d01cef01422f82c78f07d79b387
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2025-03-22 01:20:10 +0000

    Sidebar skeleton working

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Sidebar from './components/Sidebar';
+import Profile from './components/Profile';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Sidebar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
             </div>
         </Router>
