b71272dd668cad0cb0e657130260267bbffe1446
chiucarman <omgitzcarman@gmail.com>
2025-03-27 08:56:46 -0500

    WIP Create Review button

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,13 @@
 // import React from 'react';
 import Review from "./Review.tsx";
+import CreateReviewButton from "./CreateReviewButton";
 
 function Profile() {
     return (
         <div>
             <h2>Profile</h2>
             <p>Rendering from profile component!</p>
+            <CreateReviewButton />
             <Review />
         </div>
     );
