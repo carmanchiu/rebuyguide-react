88320528c2113444bb029714034171137dd2d07f
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2025-03-29 20:20:16 +0000

    configure PostgreSQL connection using envrironment variables for security

--- rebuyguide-next/lib/db.ts
+++ rebuyguide-next/lib/db.ts
@@ -1,3 +1,17 @@
 // lib/db.ts
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
+
+// Define the configuration for the database pool
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+export default pool;
