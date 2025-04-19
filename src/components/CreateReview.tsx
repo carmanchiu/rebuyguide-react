478d19876ce642ec3cdc61dee3c56df248a7c0f9
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2025-04-19 21:24:30 +0000

    StarRating component accepts props; added it to create review form

--- src/components/CreateReview.tsx
+++ src/components/CreateReview.tsx
@@ -1,29 +1,21 @@
-// Imports for the star rating
-// import React from 'react';
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
-
 // Create review button
 import CreateReviewButton from './CreateReviewButton';
+// Star rating
+import StarRating from './StarRating';
 
 function CreateReview() {
     return (
         <form>
             <div>
                 <p>Create Review template</p>
-                <FormRating />
+                <StarRating
+                    name="half-rating"
+                    readOnly={false}
+                    defaultValue={0}
+                 />
             </div>
             <CreateReviewButton>Submit</CreateReviewButton>
         </form>
     )
 }
 export default CreateReview;
-
-
-export function FormRating() {
-    return (
-        <Stack spacing={1}>
-            <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
-        </Stack>
-    );
-}
--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 
-export default function StarRating() {
+export default function StarRating({ name = "half-rating-read", defaultValue = 4.5, precision = 0.5, readOnly = true }) {
     return (
         <Stack spacing={1}>
-            <Rating name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
+            <Rating name={name} defaultValue={defaultValue} precision={precision} readOnly={readOnly} />
         </Stack>
     );
 }
